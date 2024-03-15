e572dc7ac5815dc109b1e5774ddf4a95a4dcdd0d
benjamin Faleye <bofaleye@hotmail.com>
2024-03-15 14:28:07 +0100

    updated table component

--- my-test/src/component/Table.js
+++ my-test/src/component/Table.js
@@ -4,14 +4,75 @@ import { fetchCustomerBill } from "../service/customerService"
 const Table =()=>{
     const [accountNumber, setAccountNumber] = useState('759539241');
     const [customerBillInfo, setCustomerBillInfo] = useState(null);
+
     useEffect(()=>{
        const response = fetchCustomerBill(accountNumber);
        if(response?.data){
         setCustomerBillInfo(response?.data);
        }
     },[accountNumber])
+
     return(<div>
-        <h3>Customer Information</h3>
+      <div>
+      <div className="px-4 sm:px-0">
+        <h3 className="text-base font-semibold leading-7 text-gray-900">Customer Billing Information</h3>
+        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details and application.</p>
+      </div>
+      <div className="mt-6 border-t border-gray-100">
+        <dl className="divide-y divide-gray-100">
+          <div className="px-1 py-1 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Current Outstanding balance</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot Foster</dd>
+          </div>
+          <div className="px-1 py-1 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Last Payment Date</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Backend Developer</dd>
+          </div>
+          <div className="px-1 py-1 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Last Payment Amount</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">margotfoster@example.com</dd>
+          </div>
+          <div className="px-1 py-1 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">SLT type</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">$120,000</dd>
+          </div>
+          <div className="px-1 py-1 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Constract Status</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              Fugiat
+            </dd>
+          </div>
+          
+        </dl>
+        {/* Last bill details */}
+        <dl className="divide-y divide-gray-100">
+          <div className="px-1 py-1 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Bill period</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot Foster</dd>
+          </div>
+          <div className="px-1 py-1 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Last Payment Date</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Backend Developer</dd>
+          </div>
+          <div className="px-1 py-1 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Last Payment Amount</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">margotfoster@example.com</dd>
+          </div>
+          <div className="px-1 py-1 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">SLT type</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">$120,000</dd>
+          </div>
+          <div className="px-1 py-1 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Constract Status</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              Fugiat
+            </dd>
+          </div>
+          
+        </dl>
+      </div>
+    </div>
+        {/* <h3>Customer Information</h3>
            <section>
             <h4>Payment details</h4>
             {customerBillInfo &&<div>
@@ -21,7 +82,7 @@ const Table =()=>{
                  <div>{customerBillInfo["Payment details"]["SLT type"]}</div>
                  <div>{customerBillInfo["Payment details"]["Constract Status"]}</div>
               </div>}
-           </section>
+           </section> */}
     </div>)
 }
 export default Table
